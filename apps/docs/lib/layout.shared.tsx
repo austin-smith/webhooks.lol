0ed19295202e634b3e6db8950786774e2ec0a8d3
Austin Smith <austinsmith23@gmail.com>
2026-06-06 19:46:45 -0700

    add gh link to docs

--- apps/docs/lib/layout.shared.tsx
+++ apps/docs/lib/layout.shared.tsx
@@ -2,6 +2,7 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared"
 
 export function baseOptions(): BaseLayoutProps {
   return {
+    githubUrl: "https://github.com/austin-smith/webhooks.lol",
     nav: {
       title: "webhooks.lol docs",
     },
